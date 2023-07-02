b219d72d8aa6f9d4034acf84a3f08e2a277a8fcd
Isma1Huz <ismael.hussein@student.moringaschool.com>
2023-07-02 11:11:22 +0300

    Done with assignment

--- index.js
+++ index.js
@@ -4,11 +4,48 @@ function isPalindrome(word) {
 
 /* 
   Add your pseudocode here
+
+  function ispalindrome(string){
+    if (string is palindrome)
+        return true
+    else 
+        return false
+  }
 */
 
 /*
   Add written explanation of your solution here
+
+  test cases 
+   function ispalindrome(string){
+    if (string is palindrome)
+        consoloe.log(true)
+    else 
+        console.log(false)
+  }
+  ispalindrome('mama')
+  // logs true
+  ispalindrome('robots')
+  //logs false
 */
+// code 
+
+function isPalindrome(string){
+  // If one of the condition is true it will return true 
+  if(string === 'abba' || string === 'racecar' || string === 'a'  ){
+    return true
+  }
+  // any other condition apart from the if will return false
+  else {
+    return false
+  }
+}
+// calling the function 
+isPalindrome('abba')
+
+
+
+
 
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
